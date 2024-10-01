c7d373507b8b0041bad1516596a2fad462c5838e
Jon <aljonkenfernandez36@gmail.com>
2024-10-01 22:19:55 +0800

    second commit

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// In routes/web.php
 Route::get('/', function () {
-    return view('welcome');
+    return view('tailwind');
 });
