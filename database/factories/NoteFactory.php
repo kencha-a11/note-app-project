0b0954f01f50e10eb5dda8eff31563945d45d5a5
Jon <aljonkenfernandez36@gmail.com>
2024-10-20 16:29:56 +0800

    missed merge

--- database/factories/NoteFactory.php
+++ database/factories/NoteFactory.php
@@ -5,11 +5,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
-<<<<<<< HEAD
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\odel=Note>
-=======
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Note>
->>>>>>> 64a5ab2a5988cab9cf331743d868ea1b5066a892
  */
 class NoteFactory extends Factory
 {
